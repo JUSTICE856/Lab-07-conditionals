a3ad449deec99a12704fd2e2e998f87c38e3eff2
JUSTIC856 <justice.ndukwu@mail.citytech.cuny.edu>
2021-03-26 09:57:07 -0400

    completed

---
+++ cake.py
@@ -0,0 +1,9 @@
+cakeorbust = input("Do you want cake? (yes/no)")
+
+if cakeorbust == "yes" or cakeorbust == "YES" or cakeorbust == "y" :
+    print("Have some cake!");
+elif cakeorbust == "no" or cakeorbust == "NO" or cakeorbust == "n" :
+    print("No cake for you!");
+else:
+    print("sorry, I don't understand.") #error message
+    
