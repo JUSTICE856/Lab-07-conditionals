068d70e04b26393ff841946c35a016bc570f36c6
JUSTIC856 <justice.ndukwu@mail.citytech.cuny.edu>
2021-03-26 10:09:32 -0400

    completed

---
+++ spanish.py
@@ -0,0 +1,8 @@
+E = input("give me a word in English between cat, dog, or horse")
+
+    if E == "cat" || E == "Cat") || E == "CAT":
+    print("gato")
+else if (E =="dog" || E == "Dog" || E == "DOG":
+    print("perro")
+else:
+    print("no entiendo")
